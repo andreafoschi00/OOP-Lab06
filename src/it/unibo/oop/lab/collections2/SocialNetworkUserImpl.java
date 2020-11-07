178e6169a02158d7ea05196b9dcf6017bef89581
andrea.foschi <andrea.foschi756@gmail.com>
2020-11-07 16:35:47 +0100

    Completed Collections2

--- src/it/unibo/oop/lab/collections2/SocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/collections2/SocialNetworkUserImpl.java
@@ -1,8 +1,13 @@
 package it.unibo.oop.lab.collections2;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-
+import java.util.Map;
+import java.util.Set;
 /**
  * 
  * Instructions
@@ -16,31 +21,10 @@ import java.util.List;
  * @param <U>
  *            Specific user type
  */
+
 public class SocialNetworkUserImpl<U extends User> extends UserImpl implements SocialNetworkUser<U> {
 
-    /*
-     * 
-     * [FIELDS]
-     * 
-     * Define any necessary field
-     * 
-     * In order to save the people followed by a user organized in groups, adopt
-     * a generic-type Map:
-     * 
-     * think of what type of keys and values would best suit the requirements
-     */
-
-    /*
-     * [CONSTRUCTORS]
-     * 
-     * 1) Complete the definition of the constructor below, for building a user
-     * participating in a social network, with 4 parameters, initializing:
-     * 
-     * - firstName - lastName - username - age and every other necessary field
-     * 
-     * 2) Define a further constructor where age is defaulted to -1
-     */
-
+	private final Map<String, Set<U>> friends;
     /**
      * Builds a new {@link SocialNetworkUserImpl}.
      * 
@@ -54,29 +38,42 @@ public class SocialNetworkUserImpl<U extends User> extends UserImpl implements S
      *            alias of the user, i.e. the way a user is identified on an
      *            application
      */
-    public SocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
-        super(name, surname, user, userAge);
+    public SocialNetworkUserImpl(final String firstName, final String lastName, final String username) {
+        this(firstName, lastName, username, -1);
+
     }
 
-    /*
-     * [METHODS]
-     * 
-     * Implements the methods below
-     */
+	public SocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
+        super(name, surname, user, userAge);
+        this.friends = new HashMap<>();
+    }
 
     @Override
     public boolean addFollowedUser(final String circle, final U user) {
-        return false;
+        Set<U> closeFriends = this.friends.get(circle);
+        if (closeFriends == null) {
+            closeFriends = new HashSet<>();
+            this.friends.put(circle, closeFriends);
+        }
+        return closeFriends.add(user);
     }
 
     @Override
     public Collection<U> getFollowedUsersInGroup(final String groupName) {
-        return null;
+    	final Collection<U> usersInGroups = this.friends.get(groupName);
+        if (usersInGroups != null) {
+            return new ArrayList<>(usersInGroups);
+        }
+        return Collections.emptyList();
     }
 
     @Override
     public List<U> getFollowedUsers() {
-        return null;
+    	final Set<U> followingUsers = new HashSet<>();
+        for (final Set<U> group : friends.values()) {
+            followingUsers.addAll(group);
+        }
+        return new ArrayList<>(followingUsers);
     }
 
 }
